2bf1b2303b45b3cdbe90e51a841c3d8dc91257b7
dogrod <rodrickjue@hotmail.com>
2023-03-08 18:30:20 +0800

    :ambulance: add bing proxy

--- src/pages/api/convert.ts
+++ src/pages/api/convert.ts
@@ -35,10 +35,10 @@ export default async function handler(
 
   // Insert domain rule after the last line start with DOMAIN,
   // Bing
-  // insertDomainRule(dataArr, 'bing.com')
-  // insertDomainRule(dataArr, 'www.bing.com')
-  // insertDomainRule(dataArr, 'cn.bing.com')
-  // insertDomainRule(dataArr, 'bing.net')
+  insertDomainRule(dataArr, 'bing.com')
+  insertDomainRule(dataArr, 'www.bing.com')
+  insertDomainRule(dataArr, 'cn.bing.com')
+  insertDomainRule(dataArr, 'bing.net')
 
   console.log('dataArr', dataArr)
 
